f28bdf73f47a4c1c2a43138e4285c78414b42170
a <pjh5238@naver.com>
2022-05-09 13:51:22 +0900

    Slash Command main.js add

---
+++ commands/slash command main.js
@@ -0,0 +1,4 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('your config.json path here / config.json의 경로를 여기다가 입력하세요.')
