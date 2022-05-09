aaba4a964307f1b41c9e712daead0d76fde38f4d
a <pjh5238@naver.com>
2022-05-09 12:53:58 +0900

    add main.js annotation

--- Main/main.js
+++ Main/main.js
@@ -1,6 +1,11 @@
+// Client initial setting / 클라이언트 초기 세팅
 const Discord = require('discord.js');
 const allIntents = new Discord.Intents(32767);
 const client = new Discord.Client({ allIntents });
 const { token } = require('your config.json path here.');
 
+client.once ()
+
+
+// Client login code / 클라이언트 로그인 코드
 client.login(token);
