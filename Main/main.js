8294830ab1d27dce54bdd600b1e27c45d268ab5f
a <pjh5238@naver.com>
2022-05-09 12:50:45 +0900

    main.js modified

--- Main/main.js
+++ Main/main.js
@@ -1,3 +1,6 @@
 const Discord = require('discord.js');
 const allIntents = new Discord.Intents(32767);
-const client = new Clinet({ allIntents });
+const client = new Discord.Client({ allIntents });
+const { token } = require('/Users/wogud0501/Desktop/DISCORD BOT/Discord-Bot/config/config.json');
+
+client.login(token);
