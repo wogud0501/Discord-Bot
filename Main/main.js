62651d6a2014acc0f759705451eaf907681b0638
a <pjh5238@naver.com>
2022-05-09 12:57:46 +0900

    add main.js annotation

--- Main/main.js
+++ Main/main.js
@@ -4,7 +4,11 @@ const allIntents = new Discord.Intents(32767);
 const client = new Discord.Client({ allIntents });
 const { token } = require('your config.json path here.');
 
-client.once ()
+client.once('ready', () => {
+    console.log('Ready! / 준비 완료!');
+    client.user.setActivity('Use this if you want to indicate what your bot is doing. / 당신의 봇이 하고 있는 것을 적고 싶을때 사용하시면 됩니다. 만약 이 기능이 필요 없을 경우 삭제 가능합니다.')
+    client.user.setStatus('Use this if you want to indicate what your is status. / 당신의 봇의 상태를 알려주고 싶을때 사용하시면 됩니다. 만약 이 기능이 필요 없으실 경우 지우셔도 됩니다.')
+})
 
 
 // Client login code / 클라이언트 로그인 코드
