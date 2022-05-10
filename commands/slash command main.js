4a2ce5c53191b1a842b8cd84e24e75e050b675de
a <pjh5238@naver.com>
2022-05-10 10:02:13 +0900

    A

--- commands/slash command main.js
+++ commands/slash command main.js
@@ -2,3 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('your config.json path here / config.json의 경로를 여기다가 입력하세요.')
+
+const commands = []
+
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
