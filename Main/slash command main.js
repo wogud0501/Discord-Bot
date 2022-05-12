bcaf683dc8eb01e29da0acdd120ec3a99fc30ba4
a <pjh5238@naver.com>
2022-05-12 17:30:38 +0900

    main.js modified

--- Main/slash command main.js
+++ Main/slash command main.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('your config.json path here / config.json의 경로를 여기다가 입력하세요.')
-const fs = require('fs')
+import * as fs from 'fs'
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
