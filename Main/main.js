c0846a3c85ba5e0e055cdbbd0bce9b2c36dafb2f
a <pjh5238@naver.com>
2022-06-07 11:50:58 +0900

    a

--- Main/main.js
+++ Main/main.js
@@ -1,15 +1,17 @@
+// TODO 밴 로거 제작.
 // Client initial setting / 클라이언트 초기 세팅
 const Discord = require('discord.js');
 const allIntents = new Discord.Intents(32767);
 const client = new Discord.Client({ allIntents });
 const { token } = require('your config.json path here.');
 
+// Client Response Code and Bot status Set up Code / 클라이언트 응답 코드 및 봇 상태 설정 코드
 client.once('ready', () => {
     console.log('Ready! / 준비 완료!');
     client.user.setActivity('Use this if you want to indicate what your bot is doing. / 당신의 봇이 하고 있는 것을 적고 싶을때 사용하시면 됩니다. 만약 이 기능이 필요 없을 경우 삭제 가능합니다.')
     client.user.setStatus('Use this if you want to indicate what your is status. / 당신의 봇의 상태를 알려주고 싶을때 사용하시면 됩니다. 만약 이 기능이 필요 없으실 경우 지우셔도 됩니다.')
 })
-
+// Logger Code / 로거 코드.
 client.on('messageDelete', async message => {
     if (!message.guild) return;
     const fetchedLogs = await message.guild.fetchedLogs({
@@ -30,7 +32,7 @@ client.on('messageDelete', async message => {
 })
 client.on('guildMemberRemove', async member => {
     const fetchedLogs = await member.guild.fetchAuditLogs({
-        limit : 99,
+        limit : 3,
         type : 'MEMBER_KICK',
     });
     const kickLog = fetchedLogs.entries.all();
