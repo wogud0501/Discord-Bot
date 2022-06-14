df1366a94227daa709761abf47d7c2665c18236f
a <pjh5238@naver.com>
2022-06-15 06:55:51 +0900

    a

--- commands/profile.js
+++ commands/profile.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('프로필')
-        .setDescription('자신의 프로필일 알려줍니다.ㄷ'),
+        .setDescription('자신의 프로필일 알려줍니다.'),
     async execute(interaction) {
         await interaction.reply('퐁!')
     }
